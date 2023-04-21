a34f99b011123c2a3aa8414ede69bca4934e3334
Dhevan Gangadharan <dhevanga@gmail.com>
2023-04-21 15:30:26 -0500

    minor update.

--- simulations/lumi_particles.cxx
+++ simulations/lumi_particles.cxx
@@ -48,10 +48,10 @@ TLorentzVector electron_trf; // target rest frame
 // end of spectrometer magnet: -56390
 FourVector PV = {0,0,0,0}; // Primary vertex location (x,y,z,t) in mm or mm/c
 
-int PhotonsPerEvent = 2;
-// x,y,z,t location of conversions in mm or mm/c
-vector<FourVector> pos_conversions = { FourVector(10,10,-55610,0), FourVector(-10,-10,-55610,0) };
-//vector<FourVector> pos_conversions = { FourVector(10,10,-55609,0), FourVector(-10,-10,-55609,0) };
+int PhotonsPerEvent = 1;
+// x,y,z,t location of particle(pair) origin wrt to PV in mm or mm/c
+vector<FourVector> pos_origins = { FourVector(0,0,-55609,0), FourVector(-10,-10,-55610,0) };
+//vector<FourVector> pos_origins = { FourVector(10,10,-55609,0), FourVector(-10,-10,-55609,0) };
 
 double Z = 1; // ion beam particle charge
 double electronPz = -18;
@@ -66,7 +66,7 @@ double pionZeroMass = 0.1349768;
 double pionMass = 0.13957039;
 
 
-void lumi_particles(int n_events = 1e5, bool flat=true, bool convert = true, double Egamma_start = 18.0, double Egamma_end = 18.0, string out_fname="genParticles.hepmc") {
+void lumi_particles(int n_events = 1e5, bool flat=true, bool convert = false, double Egamma_start = 10.0, double Egamma_end = 10.0, string out_fname="genParticles.hepmc") {
  
   TFile *fout = new TFile("genEventsDiagnostics.root","RECREATE");
   TH1D *BH_h1 = new TH1D("BH_h1","E",100,0,10);
@@ -149,7 +149,7 @@ void lumi_particles(int n_events = 1e5, bool flat=true, bool convert = true, dou
             FourVector(p_positron*sin(theta)*cos(phi), p_positron*sin(theta)*sin(phi), p_positron*cos(theta), E_positron), -id, 1);
         
         GenVertexPtr vertex = std::make_shared<GenVertex>();
-        vertex->set_position( pos_conversions.at( ph ) );
+        vertex->set_position( pos_origins.at( ph ) );
         vertex->add_particle_in( p_photon_in );
         vertex->add_particle_out( p3 );
         vertex->add_particle_out( p4 );
@@ -163,7 +163,7 @@ void lumi_particles(int n_events = 1e5, bool flat=true, bool convert = true, dou
         GenParticlePtr p_photon_out = std::make_shared<GenParticle>( 
             FourVector(E*sin(theta)*cos(phi), E*sin(theta)*sin(phi), E*cos(theta), E), id, 1);
         GenVertexPtr vertex = std::make_shared<GenVertex>();
-        vertex->set_position( pos_conversions.at( ph ) );
+        vertex->set_position( pos_origins.at( ph ) );
         vertex->add_particle_in( p_photon_in );
         vertex->add_particle_out( p_photon_out );
         evt.add_vertex( vertex );
