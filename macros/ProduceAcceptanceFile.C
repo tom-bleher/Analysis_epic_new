5bdd7f7206895cccccda1ef04c84bac6c6205ee3
Dhevan Gangadharan <dhevanga@gmail.com>
2024-01-06 17:49:01 -0600

    minor macro update

--- macros/ProduceAcceptanceFile.C
+++ macros/ProduceAcceptanceFile.C
@@ -4,7 +4,7 @@
 #include<TH1.h>
 #include<TH3.h>
 
-void ProduceAcceptanceFile( TString fname = "../eventAnalysis/ConvMiddle/MergedOutput.root" )
+void ProduceAcceptanceFile( TString fname = "../eventAnalysis/ConvMiddleQuarterCurrent/MergedOutput.root" )
 {
 
   TFile *f = new TFile(fname, "READ");
@@ -30,5 +30,31 @@ void ProduceAcceptanceFile( TString fname = "../eventAnalysis/ConvMiddle/MergedO
     h->Write();
   }
 
+  // In terms of scattered electron energy
 
-}// void close
+  TH1D *trackerTop_SE = new TH1D("trackerTop_SE", ";Electron energy E (GeV);PS acceptance", trackerTop->GetNbinsX(), trackerTop->GetXaxis()->GetXmin(), trackerTop->GetXaxis()->GetXmax() );
+  TH1D *trackerCoinc_SE = new TH1D("trackerCoinc_SE", ";Electron energy E (GeV);PS acceptance", trackerTop->GetNbinsX(), trackerTop->GetXaxis()->GetXmin(), trackerTop->GetXaxis()->GetXmax() );
+  trackerTop_SE->SetMarkerStyle(20);
+  trackerCoinc_SE->SetMarkerStyle(20);
+
+  for( int bin = 1; bin <= trackerTop->GetNbinsX(); bin++ ) {
+    
+    double SE_Energy = 18 - trackerTop->GetXaxis()->GetBinCenter(bin);
+    if( trackerTop->GetBinContent(bin) > 0.0001 ) {
+      trackerTop_SE->Fill( SE_Energy, trackerTop->GetBinContent(bin) );
+      trackerTop_SE->SetBinError(bin, 0.00001);
+    }
+
+    if( trackerCoinc->GetBinContent(bin) > 0.0001 ) {
+      trackerCoinc_SE->Fill( SE_Energy, trackerCoinc->GetBinContent(bin) );
+      trackerCoinc_SE->SetBinError(bin, 0.00001);
+    }
+  }
+
+  //trackerTop_SE->Draw("hist p");
+  trackerCoinc_SE->Draw("hist p");
+
+  trackerTop_SE->Write();
+  trackerCoinc_SE->Write();
+
+}
