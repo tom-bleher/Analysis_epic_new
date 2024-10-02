fefc663534fa237698f6eb0580ec054a9e9104c0
TomBleher <babatheninja@gmail.com>
2024-10-03 01:49:35 +0200

    Implemented README function and attempted to fix XML writing by copying epic directly

--- simulations/createPixSimFiles_.py
+++ simulations/createPixSimFiles_.py
@@ -4,7 +4,6 @@ Created on Fri Sep  6 15:33:11 2024
 
 @author: tombleher
 """
-
 import os
 import shutil
 from datetime import datetime
@@ -13,6 +12,7 @@ import re
 import json
 import concurrent.futures
 import subprocess
+import xml.etree.ElementTree as ET
 
 class HandleEIC(object):
     
@@ -20,9 +20,9 @@ class HandleEIC(object):
         self.setup_var()
         self.setup_paths()
         self.pixelSize_pairs = self.setup_json()
-            
-        for dx, dy in self.pixelSize_pairs:
-            self.pixelSize_dx, self.pixelSize_dy = dx, dy
+
+        for px_dx, px_dy in self.pixelSize_pairs:
+            self.pixelSize_dx, self.pixelSize_dy = px_dx, px_dy
             self.setup_file_specifics(self.pixelSize_dx, self.pixelSize_dy)
 
     def setup_var(self) -> None:
@@ -38,25 +38,22 @@ class HandleEIC(object):
         """
         Method for setting paths for input, output, and other resources.
         """
+        self.run_path = os.getcwd()
+        self.base_epic_path = f"/data/tomble/eic/epic"
+        self.main_xml_path = f"{self.base_epic_path}/install/share/epic/epic_ip6_extended.xml"
+        self.createGenFiles_path = f"{self.run_path}/createGenFiles.py" # get BH value for generated hepmc files (zero or one)
+        self.energies = [file for file in sorted(os.listdir(f"{self.run_path}/genEvents/results/")) if self.file_type in file]  
+
         self.in_path = ""
         self.out_path = ""
         if len(sys.argv) > 1: 
             self.in_path = "/" + sys.argv[1]
         if len(sys.argv) > 2: 
             self.out_path = "/" + sys.argv[2]
-        
-        # directories genEvents and simEvents needs to exist and manually changed here
-        self.epic_path = r"/data/tomble/eic/epic/install/share/epic/epic_ip6_extended.xml"
-        self.base_px_xml_path = r"/data/tomble/eic/epic/install/share/epic/compact/far_backward/definitions.xml"
-        self.px_json_path = os.getcwd() # json is located in the same path as this python file
-        self.gen_path = r"/data/tomble/Analysis_epic_new/simulations/genEvents/results/"
-        self.energies = [file for file in sorted(os.listdir(self.gen_path)) if self.file_type in file]  
-        self.det_path = os.environ['DETECTOR_PATH']
-        self.compact_path = self.det_path + '/compact'
 
         if not self.out_path:
             self.out_path = self.in_path
-       
+
         self.gen_path = f"genEvents{self.in_path}"
         self.sim_path = f"simEvents{self.out_path}"
         self.sim_path_items = os.listdir(self.sim_path)
@@ -66,13 +63,51 @@ class HandleEIC(object):
         os.makedirs(simEvents_path, exist_ok=True)
         self.set_permission(simEvents_path)
 
+    def setup_readme(self) -> None:
+        
+        # define path for readme file 
+        self.readme_path = os.path.join(self.SimBackUpPath, "README.txt")
+        
+        # call the function to read the BH value from the function
+        self.BH_val = self.get_BH_val()
+
+        # get energy levels from files names of genEvents
+        self.photon_energy_vals = [
+            '.'.join(file.split('_')[1].split('.', 2)[:2]) 
+            for file in self.energies
+        ]
+
+        # write readme content to the file
+        with open(self.readme_path, 'w') as file:
+            file.write(f'file_type: {self.file_type}\n')
+            file.write(f'Number of Particles: {self.num_particles}')
+            file.write(f'Pixel Value Pairs: {self.pixelSize_pairs}\n')
+            file.write(f'BH: {self.BH_val}\n')
+            file.write(f'Energies: {self.photon_energy_vals}\n')
+
+    def get_BH_val(self):
+
+        # open the path storing the createGenFiles.py file
+        with open(self.createGenFiles_path, 'r') as file:
+            content = file.read()
+            
+        # use a regex to find the line where BH is defined
+        match = re.search(r'BH\s*=\s*(.+)', content)
+        
+        # if we found BH in the file, we return the value
+        if match:
+            value = match.group(1).strip()
+            return value
+        else:
+            raise ValueError("Could not find a value for 'BH' in the content of the file.")
+
     def setup_json(self) -> list[tuple]:
         """
         Method for setting up JSON file containing pixel size.
         If the JSON file doesn't exist or is incorrect, a new one is created with default pixel sizes.
         """
         self.px_dict = {}
-        self.px_json_path = os.path.join(self.px_json_path, 'pixel_data.json')
+        self.px_json_path = os.path.join(self.run_path, 'pixel_data.json')
         try:
             # Try to read and load the JSON file
             with open(self.px_json_path,'r') as file:
@@ -94,50 +129,59 @@ class HandleEIC(object):
             self.px_pairs = self.px_dict['LumiSpecTracker_pixelSize']
         return self.px_pairs
 
-    def setup_file_specifics(self, dx: float, dy: float) -> None: 
+    def setup_file_specifics(self, curr_px_dx: float, curr_px_dy: float) -> None: 
         """
         Method for setting up file specifics such as creating respective pixel folders, changing definitions xml and 
         looping over all energy levels and saving ddsim commands.
         """
         # create respective px folders and their compact folders
-        curr_pix_sim_path = os.path.join(self.sim_path, f"{dx}x{dy}px") 
-        curr_compact_path = os.path.join(curr_pix_sim_path, "compact")
+        curr_pix_path = os.path.join(self.sim_path, f"{curr_px_dx}x{curr_px_dy}px") 
+        curr_epic_path = os.path.join(curr_pix_path, "epic")
+        curr_epic_ip6_path = os.path.join(curr_epic_path, "epic_ip6_extended.xml")
 
         # create directory for px if it doesn't exist
-        os.makedirs(curr_pix_sim_path, exist_ok=True) 
-        os.makedirs(curr_compact_path, exist_ok=True) 
+        os.makedirs(curr_pix_path, exist_ok=True) 
+        os.makedirs(curr_epic_path, exist_ok=True) 
 
         # set permissions
-        self.set_permission(curr_compact_path)
-        self.set_permission(curr_pix_sim_path)
+        self.set_permission(curr_pix_path)
+        self.set_permission(curr_epic_path)
+
+        # call fucntion to writing the readme file containing the information
+        self.setup_readme()
 
         # copy epic compact to each respective px folder for parameter reference 
-        shutil.copytree(self.compact_path, curr_compact_path, dirs_exist_ok=True)
+        shutil.copytree(self.base_epic_path, curr_epic_path, dirs_exist_ok=True)
 
-        # change definitions xml for each pixel folder 
-        self.write_xml(dx, dy, os.path.join(curr_compact_path, 'definitions.xml')) 
+        # rewrite {DETECTOR_PATH} and /compact/ for current                                 
+        self.rewrite_xml_tree(curr_epic_path, curr_pix_path)
 
         # loop over all energy levels and save ddsim commands
-        self.setup_queue(curr_pix_sim_path)
+        self.setup_queue(curr_epic_ip6_path, curr_pix_path)
 
-    # for far backwards detectors definition files
-    def write_xml(self, px_dx: float, px_dy: float, px_file: str) -> None:
-        """
-        Method for updating the XML file around pixel values.
-        """
-        # loop over the copied files and replace the default value with the user input
-        with open(px_file, "r+") as file:
-            content = file.read()
-            # replace the default value with the new value
-            content = content.replace(f'<constant name="LumiSpecTracker_pixelSize_dx" value="{self.def_px_dx}*mm"/>',\
-                                       f'<constant name="LumiSpecTracker_pixelSize_dx" value="{px_dx}*mm"/>')
-            content = content.replace(f'<constant name="LumiSpecTracker_pixelSize_dy" value="{self.def_px_dy}*mm"/>',\
-                                       f'<constant name="LumiSpecTracker_pixelSize_dy" value="{px_dy}*mm"/>')
-            file.seek(0)
-            file.write(content)
-            file.truncate()
+    def rewrite_xml_tree(self, curr_epic_path, curr_px_dx, curr_px_dy):
 
-    def setup_queue(self, curr_pix_sim_path: str) -> dict:
+        # for every "{DETECTOR_PATH}" in copied epic XMLs, we replace with the path for the current compact pixel path 
+        # and for every compact path we replace with our new compact path 
+        
+        # iterate over all XML files in the copied epic directory
+        for subdir, dirs, files in os.walk(curr_epic_path):
+            for filename in files:
+                filepath = subdir + os.sep + filename
+                if filepath.endswith(".xml"):
+                    tree = ET.parse(filepath)
+                    root = tree.getroot()
+                    for elem in root.iter():
+                        if elem.text:
+                            if "{DETECTOR_PATH}" in elem.text:
+                                elem.text = elem.text.replace("{DETECTOR_PATH}", f"{curr_epic_path}")
+                            elif "<constant name='LumiSpecTracker_pixelSize_dx' value='" in elem.text:
+                                elem.text = re.sub(r"<constant name='LumiSpecTracker_pixelSize_dx' value='.*?'>", f"<constant name='LumiSpecTracker_pixelSize_dx' value='{curr_px_dx}*mm'>", elem.text)
+                            elif "<constant name='LumiSpecTracker_pixelSize_dy' value='" in elem.text:
+                                elem.text = re.sub(r"<constant name='LumiSpecTracker_pixelSize_dy' value='.*?'>", f"<constant name='LumiSpecTracker_pixelSize_dy' value='{curr_px_dy}*mm'>", elem.text)
+                        tree.write(filepath)
+
+    def setup_queue(self, curr_epic_ip6_path, curr_pix_path: str) -> dict:
         """
         Method for setting up the queue of commands, each for executing ddsim.
         """
@@ -145,7 +189,7 @@ class HandleEIC(object):
         for file in self.energies: 
             inFile = self.gen_path + "/results/" + file 
             fileNum = re.search("\d+\.+\d\.", inFile).group() 
-            cmd = f"ddsim --inputFiles {inFile} --outputFile {curr_pix_sim_path}/output_{fileNum}edm4hep.root --compactFile {self.epic_path} -N {self.num_particles}"
+            cmd = f"ddsim --inputFiles {inFile} --outputFile {curr_pix_path}/output_{fileNum}edm4hep.root --compactFile {curr_epic_ip6_path} -N {self.num_particles}"
             
             # each file path maps to its associated command
             self.run_queue[inFile] = cmd
