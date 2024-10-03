8b2180d350055abd60a4dbc8ad3d1c659d1396a4
TomBleher <babatheninja@gmail.com>
2024-10-03 21:30:48 +0200

    Attempt to fix path bug

--- simulations/createPixSimFiles_.py
+++ simulations/createPixSimFiles_.py
@@ -17,32 +17,32 @@ import xml.etree.ElementTree as ET
 class HandleEIC(object):
     
     def __init__(self) -> None:
-        self.setup_var()
-        self.setup_paths()
-        self.pixelSize_pairs = self.setup_json()
+        self.init_var()
+        self.init_path()
+        self.pixel_sizes = self.setup_json()
 
-        for px_dx, px_dy in self.pixelSize_pairs:
-            self.pixelSize_dx, self.pixelSize_dy = px_dx, px_dy
-            self.setup_file_specifics(self.pixelSize_dx, self.pixelSize_dy)
+        for dx, dy in self.pixel_sizes:
+            self.dx, self.dy = dx, dy
+            self.prepare_files(self.dx, self.dy)
 
-    def setup_var(self) -> None:
+    def init_var(self) -> None:
         """
         Method for setting variables that control the processing.
         """
         self.file_type = "beamEffectsElectrons" # or "idealElectrons" 
         self.num_particles = 5000
-        self.def_px_dx = 0.1 # res in mm
-        self.def_px_dy = 0.1 # res in mm
+        self.default_dx = 0.1 # res in mm
+        self.default_dy = 0.1 # res in mm
 
-    def setup_paths(self) -> None:
+    def init_path(self) -> None:
         """
         Method for setting paths for input, output, and other resources.
         """
-        self.run_path = os.getcwd()
+        self.execution_path = os.getcwd()
         self.base_epic_path = os.environ['DETECTOR_PATH'] # /data/tomble/eic/epic/install/share/epic
         self.main_xml_path = f"{self.base_epic_path}/install/share/epic/epic_ip6_extended.xml"
-        self.createGenFiles_path = f"{self.run_path}/createGenFiles.py" # get BH value for generated hepmc files (zero or one)
-        self.energies = [file for file in sorted(os.listdir(f"{self.run_path}/genEvents/results/")) if self.file_type in file]  
+        self.createGenFiles_path = f"{self.execution_path}/createGenFiles.py" # get BH value for generated hepmc files (zero or one)
+        self.energy_levels  = [file for file in sorted(os.listdir(f"{self.execution_path}/genEvents/results/")) if self.file_type in file]  
 
         self.in_path = ""
         self.out_path = ""
@@ -54,16 +54,16 @@ class HandleEIC(object):
         if not self.out_path:
             self.out_path = self.in_path
 
-        self.gen_path = f"genEvents{self.in_path}"
-        self.sim_path = f"simEvents{self.out_path}"
-        self.sim_path_items = os.listdir(self.sim_path)
-
-        # create the path where the simulation file backup will go
-        self.SimBackUpPath = os.path.join(self.sim_path, datetime.now().strftime("%Y%m%d_%H%M%S"))
+        self.genEvents_path  = f"genEvents{self.in_path}"
+        self.simEvents_path  = f"simEvents{self.out_path}"
 
         # if there is no simEvents then create it
-        simEvents_path = os.path.join(os.getcwd(), self.sim_path)
+        simEvents_path = os.path.join(os.getcwd(), self.simEvents_path)
         os.makedirs(simEvents_path, exist_ok=True)
+        self.simEvents_items = os.listdir(self.simEvents_path)
+
+        # create the path where the simulation file backup will go
+        self.backup_path  = os.path.join(self.simEvents_path , datetime.now().strftime("%Y%m%d_%H%M%S"))
         self.set_permission(simEvents_path)
 
     def get_BH_val(self):
@@ -88,7 +88,7 @@ class HandleEIC(object):
         If the JSON file doesn't exist or is incorrect, a new one is created with default pixel sizes.
         """
         self.px_dict = {}
-        self.px_json_path = os.path.join(self.run_path, 'pixel_data.json')
+        self.px_json_path = os.path.join(self.execution_path, 'pixel_data.json')
         try:
             # Try to read and load the JSON file
             with open(self.px_json_path,'r') as file:
@@ -103,20 +103,20 @@ class HandleEIC(object):
                 raise ValueError("Invalid JSON file...")
         except:
             # Create a new JSON with default values
-            self.px_dict = {"LumiSpecTracker_pixelSize": [[self.def_px_dx, self.def_px_dy]]}
+            self.px_dict = {"LumiSpecTracker_pixelSize": [[self.default_dx, self.default_dy]]}
             with open(self.px_json_path,'w') as file:
                 json.dump(self.px_dict, file)
             print(f"No valid JSON found. Created JSON file at {self.px_json_path}. Edit the pairs in the JSON to specify your desired values.")
             self.px_pairs = self.px_dict['LumiSpecTracker_pixelSize']
         return self.px_pairs
 
-    def setup_file_specifics(self, curr_px_dx: float, curr_px_dy: float) -> None: 
+    def prepare_files(self, curr_px_dx: float, curr_px_dy: float) -> None: 
         """
         Method for setting up file specifics such as creating respective pixel folders, changing definitions xml and 
         looping over all energy levels and saving ddsim commands.
         """
         # create respective px folders and their compact folders
-        curr_pix_path = os.path.join(self.sim_path, f"{curr_px_dx}x{curr_px_dy}px") 
+        curr_pix_path = os.path.join(self.simEvents_path , f"{curr_px_dx}x{curr_px_dy}px") 
         curr_epic_path = os.path.join(curr_pix_path, "epic")
         curr_epic_ip6_path = os.path.join(curr_epic_path, "epic_ip6_extended.xml")
 
@@ -164,8 +164,8 @@ class HandleEIC(object):
         Method for setting up the queue of commands, each for executing ddsim.
         """
         self.run_queue = {} # init dict to hold commands per file
-        for file in self.energies: 
-            inFile = self.gen_path + "/results/" + file 
+        for file in self.energy_levels : 
+            inFile = self.genEvents_path  + "/results/" + file 
             fileNum = re.search("\d+\.+\d\.", inFile).group() 
             cmd = f"ddsim --inputFiles {inFile} --outputFile {curr_pix_path}/output_{fileNum}edm4hep.root --compactFile {curr_epic_ip6_path} -N {self.num_particles}"
             
@@ -206,7 +206,7 @@ class HandleEIC(object):
     def setup_readme(self) -> None:
         
         # define path for readme file 
-        self.readme_path = os.path.join(self.SimBackUpPath, "README.txt")
+        self.readme_path = os.path.join(self.backup_path , "README.txt")
         
         # call the function to read the BH value from the function
         self.BH_val = self.get_BH_val()
@@ -214,16 +214,16 @@ class HandleEIC(object):
         # get energy levels from files names of genEvents
         self.photon_energy_vals = [
             '.'.join(file.split('_')[1].split('.', 2)[:2]) 
-            for file in self.energies
+            for file in self.energy_levels 
         ]
 
         # write readme content to the file
         with open(self.readme_path, 'a') as file:
             file.write(f'file_type: {self.file_type}\n')
             file.write(f'Number of Particles: {self.num_particles}')
-            file.write(f'Pixel Value Pairs: {self.pixelSize_pairs}\n')
+            file.write(f'Pixel Value Pairs: {self.pixel_sizes}\n')
             file.write(f'BH: {self.BH_val}\n')
-            file.write(f'Energies: {self.photon_energy_vals}\n')
+            file.write(f'Energy Levels : {self.photon_energy_vals}\n')
 
     def mk_backup(self) -> None:
         """
@@ -231,17 +231,17 @@ class HandleEIC(object):
         """
 
         # create a backup for this run
-        if len(self.sim_path_items) > 0:
-            os.makedirs(self.SimBackUpPath, exist_ok=True)
-            self.set_permission(self.SimBackUpPath)
-            print(f"Created new backup directory in {self.SimBackUpPath}")
+        if len(self.simEvents_items) > 0:
+            os.makedirs(self.backup_path , exist_ok=True)
+            self.set_permission(self.backup_path )
+            print(f"Created new backup directory in {self.backup_path }")
 
             # move files and pixel folders to backup
-            for item in self.sim_path_items:
-                item_path = os.path.join(self.sim_path, item)
+            for item in self.simEvents_items:
+                item_path = os.path.join(self.simEvents_path , item)
 
                 if os.path.isfile(item_path) or (os.path.isdir(item_path) and "px" in item):
-                    shutil.move(item_path, self.SimBackUpPath)
+                    shutil.move(item_path, self.backup_path )
 
         # call fucntion to writing the readme file containing the information
         self.setup_readme()
