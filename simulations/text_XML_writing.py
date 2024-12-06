daa8ee03f85d29c9cdfa1448b94c9a6a72661a5a
Tom Bleher <72304996+tom-bleher@users.noreply.github.com>
2024-12-06 22:48:56 +0200

    Went back to old XML and new path for test

--- simulations/text_XML_writing.py
+++ simulations/text_XML_writing.py
@@ -23,45 +23,36 @@ import subprocess
 import multiprocessing
 import stat
 
-def rewrite_xml_tree(curr_epic_path, curr_px_dx, curr_px_dy):
+def rewrite_xml_tree(parent_dir, curr_px_dx, curr_px_dy):
     """
-    Rewrite the pixel values and replace ${DETECTOR_PATH} in all XML files 
-    for the Epic detector.
+    Method for rewriting desired pixel values for all XML files of the Epic 
+    detector. For every "{DETECTOR_PATH}" in copied epic XMLs, we replace with the path 
+    for the current compact pixel path, and for every compact path 
+    we replace with our new compact path 
 
     Args:
-        curr_epic_path (str): Root directory containing XML files.
-        curr_px_dx (float): New pixel size dx in mm.
-        curr_px_dy (float): New pixel size dy in mm.
+        parent_dir
+        curr_px_dx
+        curr_px_dy
     """
-    for subdir, dirs, files in os.walk(curr_epic_path):
+    # iterate over all XML files in the copied epic directory
+    for subdir, dirs, files in os.walk(parent_dir):
+        os.chmod(filepath, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
         for filename in files:
-            filepath = os.path.join(subdir, filename)
+            filepath = subdir + os.sep + filename
             if filepath.endswith(".xml"):
-                try:
-                    # Ensure the file is readable and writable
-                    os.chmod(filepath, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
-                    
-                    # Parse the XML file
-                    tree = ET.parse(filepath)
-                    root = tree.getroot()
-                    
-                    # Modify the XML
-                    for elem in root.iter():
-                        # Modify pixel size constants
-                        if "constant" in elem.tag and 'name' in elem.attrib:
-                            if elem.attrib['name'] == "LumiSpecTracker_pixelSize_dx":
-                                elem.attrib['value'] = f"{curr_px_dx}*mm"
-                            elif elem.attrib['name'] == "LumiSpecTracker_pixelSize_dy":
-                                elem.attrib['value'] = f"{curr_px_dy}*mm"
+                tree = ET.parse(filepath)
+                root = tree.getroot()
+                for elem in root.iter():
+                    if "constant" in elem.tag and 'name' in elem.keys():
+                        if elem.attrib['name'] == "LumiSpecTracker_pixelSize_dx":
+                            elem.attrib['value'] = f"{curr_px_dx}*mm"
+                        elif elem.attrib['name'] == "LumiSpecTracker_pixelSize_dy":
+                            elem.attrib['value'] = f"{curr_px_dy}*mm"
+            
+                    if elem.text and "${DETECTOR_PATH}" in elem.text:
+                        elem.text = elem.text.replace("${DETECTOR_PATH}", curr_epic_path)
                         
-                        # Replace ${DETECTOR_PATH}
-                        if elem.text and "${DETECTOR_PATH}" in elem.text:
-                            elem.text = elem.text.replace("${DETECTOR_PATH}", curr_epic_path)
-                    
-                    # Save changes back to the file
-                    tree.write(filepath)
-                
-                except Exception as e:
-                    print(f"Error processing file {filepath}: {e}")
+                tree.write(filepath)
 
-rewrite_xml_tree("/data/tomble/Analysis_epic_new/simulations/simEvents/20241206_214112/2.0x0.1px/epic_sim", "3.0", "0.5")
+rewrite_xml_tree("/data/tomble/Analysis_epic_new/simulations/simEvents/20241206_212905/2.0x0.1px/epic_sim", "3.0", "0.5")
