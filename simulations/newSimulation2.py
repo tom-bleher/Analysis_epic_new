9406b04665370cc64bd2279a50361867a0842c3e
Tom Bleher <72304996+tom-bleher@users.noreply.github.com>
2024-12-13 22:38:29 +0200

    minor import fixes

--- simulations/newSimulation2.py
+++ simulations/newSimulation2.py
@@ -14,6 +14,7 @@ import subprocess
 import multiprocessing
 import logging
 from typing import Dict, List, Tuple
+import concurrent
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 class HandleEIC(object):
