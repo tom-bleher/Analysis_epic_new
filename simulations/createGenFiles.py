1a1f69ccdd48d66e09a35b5f009fc1e08ee90410
tom-bleher <babatheninja@gmail.com>
2025-02-06 11:00:37 +0200

    Update createGenFiles.py to have "energies" variable

--- simulations/createGenFiles.py
+++ simulations/createGenFiles.py
@@ -10,6 +10,8 @@ location = "POS.ConvMiddle"
 
 genPath = "genEvents"
 
+energies = [10,15,20,25,30] # GeV
+
 if len(sys.argv) > 1: 
   genPath = genPath + "/" + sys.argv[1]
 else:
@@ -21,23 +23,21 @@ if len(os.listdir(genPath)) != 0:
   print("{0} directory not empty.  Clear directory".format(genPath))
   exit()
 
-#for n in range(10):
-if BH == 1: # BH E spectrum
+for n in energies:
   ID = n # just the index of statistically independent sample
-  cmd = "root -q 'lumi_particles.cxx(1e4,false,false,false,4,18,\"{1}/idealPhotonsAtIP_{0}.hepmc\")'".format(ID,genPath)
-else: # flat E spectrum
-  ID = 20 #1 + n/2 # Energy of photon
-  cmd = "root -q 'lumi_particles.cxx(1e4,true,false,false,{0},{0},\"{1}/idealPhotonsAtIP_{0}.hepmc\")'".format(ID,genPath)
+  if BH == 1: # BH E spectrum
+    cmd = "root -q 'lumi_particles.cxx(1e4,false,false,false,4,18,\"{1}/idealPhotonsAtIP_{0}.hepmc\")'".format(ID,genPath)
+  else: # flat E spectrum
+    cmd = "root -q 'lumi_particles.cxx(1e4,true,false,false,{0},{0},\"{1}/idealPhotonsAtIP_{0}.hepmc\")'".format(ID,genPath)
 
-os.system(cmd)
+  os.system(cmd)
 
-# beam effects
-cmd = "abconv {1}/idealPhotonsAtIP_{0}.hepmc --plot-off -o {1}/beamEffectsPhotonsAtIP_{0}".format(ID,genPath)
-os.system(cmd)
+  # beam effects
+  cmd = "abconv {1}/idealPhotonsAtIP_{0}.hepmc --plot-off -o {1}/beamEffectsPhotonsAtIP_{0}".format(ID,genPath)
+  os.system(cmd)
 
-# propagate to certain locations, specified as last argument
-cmd = "root -b 'PropagateAndConvert.cxx(\"{1}/beamEffectsPhotonsAtIP_{0}.hepmc\",\"{1}/beamEffectsElectrons_{0}.hepmc\",{2})'".format(ID,genPath,location)
-os.system(cmd)
-cmd = "root -b 'PropagateAndConvert.cxx(\"{1}/idealPhotonsAtIP_{0}.hepmc\",\"{1}/idealElectrons_{0}.hepmc\",{2})'".format(ID,genPath,location)
-os.system(cmd)
-
+  # propagate to certain locations, specified as last argument
+  cmd = "root -b 'PropagateAndConvert.cxx(\"{1}/beamEffectsPhotonsAtIP_{0}.hepmc\",\"{1}/beamEffectsElectrons_{0}.hepmc\",{2})'".format(ID,genPath,location)
+  os.system(cmd)
+  cmd = "root -b 'PropagateAndConvert.cxx(\"{1}/idealPhotonsAtIP_{0}.hepmc\",\"{1}/idealElectrons_{0}.hepmc\",{2})'".format(ID,genPath,location)
+  os.system(cmd)
